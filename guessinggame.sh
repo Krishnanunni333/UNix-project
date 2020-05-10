f8e48bdb598df7e07aa108654c0244ddf7b3b5d0
kittu <kittuofficial8@gmail.com>
2020-05-10 23:07:42 +0530

    for assignment in UNIX

---
+++ guessinggame.sh
@@ -0,0 +1,18 @@
+k=$PWD
+a=$(ls | wc -l)
+while [[ true ]]
+do
+    echo "enter a number to guess"
+    read s
+    if [[ $a -eq $s ]]
+    then
+        echo "Congratulations"
+        break
+    elif [[ $s -gt $a ]]
+    then 
+        echo "Your guess  is high"
+    else
+        echo "Your guess  is low"
+    fi
+done
+    
