0e8370c6f69d57701165073acb45c4f5b9d32113
Krishnanunni333 <46400630+Krishnanunni333@users.noreply.github.com>
2020-05-11 20:42:30 +0530

    for project

---
+++ guessinggame.sh
@@ -0,0 +1,23 @@
+function testfunction()
+{
+local a=$(ls | wc -l)
+echo $a
+}
+echo "<----Your gussing starts---->"
+k=$(testfunction)
+while [[ true ]]
+do
+    echo "Enter a number to guess"
+    read s
+    if [[ $k -eq $s ]]
+    then
+        echo "Congratulations!!!"
+        break
+    elif [[ $s -gt $k ]]
+    then 
+        echo "Your guess  is high"
+    else
+        echo "Your guess  is low"
+    fi
+done
+  
